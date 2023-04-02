f4e6ec27ae4b662b72041840ee3a90737dd09875
jxr2006 <xxx.com>
2023-04-02 13:15:22 +0800

    .

--- Assets/CustomMap/LevelDataBuildView.cs
+++ Assets/CustomMap/LevelDataBuildView.cs
@@ -17,15 +17,26 @@ public class LevelDataBuildView : ScriptableObject, IResetable
 
     private LevelDataResource levelDataResource;
 
+    private string m_outPut;
+
     [Button("从文本加载")]
     public void LoadFromRaw([LabelText("文本")] string rawJson)
     {
         levelDataResource.levelData = JsonConvert.DeserializeObject<LevelData>(rawJson);
+        Debug.Log("Deserialize done");
     }
 
     [Button("从文件加载")]
     public void LoadFromFile([LabelText("路径")] string path)
     {
         levelDataResource.levelData = JsonConvert.DeserializeObject<LevelData>(File.ReadAllText(path));
+        Debug.Log("Deserialize done");
+    }
+
+    [Button("生成到文件")]
+    public void DumpToRaw([LabelText("路径")] string path)
+    {
+        m_outPut = JsonConvert.SerializeObject(levelDataResource.levelData, new JsonSerializerSettings() { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
+        File.WriteAllText(path, m_outPut);
     }
 }
